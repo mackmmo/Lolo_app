e86fec06e48861028698362bb229cc13d541a9b1
mackmmo <moorem2360@gmail.com>
2026-01-26 11:54:01 -0700

    added env var: ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -157,3 +157,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ALLOWED_HOSTS = [
+    "lolo-app-1.onrender.com",
+]
