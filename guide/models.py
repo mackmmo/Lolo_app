fa67eef173883f1bca194e2df60b80b49b4109de
mackmmo <moorem2360@gmail.com>
2026-02-19 11:14:22 -0700

    update

--- guide/models.py
+++ guide/models.py
@@ -2,7 +2,7 @@
 from django.db import models  # regular Django models
 
 class Sector(models.Model):
-    sector_id = models.IntegerField()
+    sector_id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=100)
     description = models.TextField()
 
