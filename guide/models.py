91fe34925c051fd9a751a57a4bcbd9f190c8add0
mackmmo <moorem2360@gmail.com>
2026-02-19 10:39:48 -0700

    update

--- guide/models.py
+++ guide/models.py
@@ -5,7 +5,7 @@ from django.contrib.gis.db import models # spatial models
 class Sector(models.Model):
     sector_id = models.IntegerField()
     name = models.CharField(max_length=100)
-    geom = models.GeometryField()
+    description = models.TextField()
 
     class Meta:
         db_table = 'sector'  # <- force Django to use the existing table
