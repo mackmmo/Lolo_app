656dc0a63a6dbb2c2d75b1f5d053bb35ed451e72
mackmmo <moorem2360@gmail.com>
2026-02-19 10:48:45 -0700

    update

--- guide/models.py
+++ guide/models.py
@@ -1,11 +1,14 @@
 
-from django.db import models  # regular Django models
-from django.contrib.gis.db import models # spatial models
+from django.db import models as djmodels  # regular Django models
+from django.contrib.gis.db import models as GISmodels # spatial models
 
-class Sector(models.Model):
-    sector_id = models.IntegerField()
-    name = models.CharField(max_length=100)
-    description = models.TextField()
+class Sector(djmodels.Model):
+    sector_id = djmodels.IntegerField()
+    name = djmodels.CharField(max_length=100)
+    description = djmodels.TextField()
 
     class Meta:
         db_table = 'sector'  # <- force Django to use the existing table
+
+class FutureModes(GISmodels.Model):
+    geom = GISmodels.GeometryField()
