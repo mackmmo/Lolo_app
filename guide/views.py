5af26a74620d614c0723686ac7825a10ac5264ec
mackmmo <moorem2360@gmail.com>
2026-03-17 13:23:52 -0600

    arealistview

--- guide/views.py
+++ guide/views.py
@@ -1,9 +1,13 @@
 # guide/views.py
 from rest_framework import generics
-from .models import Sector
+from .models import Sector, Area
 from .serializers import SectorSerializer
 from django.http import JsonResponse
 
 class SectorListView(generics.ListAPIView):
     queryset = Sector.objects.all().order_by('sector_id')
     serializer_class = SectorSerializer
+
+class AreaListView(generics.ListAPIView):
+    queryset = Area.objects.all().order_by('area_id')
+    serializer_class = AreaSerializer
