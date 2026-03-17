fc6f53182120442c74738d9ad866caac3e086b5f
mackmmo <moorem2360@gmail.com>
2026-03-17 11:51:43 -0600

    continueing editing from first commit today

--- guide/serializers.py
+++ guide/serializers.py
@@ -4,30 +4,4 @@ from .models import Sector
 class SectorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Sector
-        fields = ['sector_id', 'name', 'description']
-'''
-class AreaSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Area
-        fields = ['area_id', 'sector', 'boundary', 'centroid', 'name', 'description']
-
-class SubAreaSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = SubArea
-        fields = ['subarea_id', 'area', 'name', 'boundary', 'centroid', 'description']
-
-class RoutesSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Routes
-        fields = ['route_id', 'subarea', 'name', 'grade', 'danger_rating', 'fa', 'fa_year', 'coordinates', 'description']
-
-class RoadsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Roads
-        fields = ['road_id', 'name', 'description', 'path', 'distance']
-
-class ParkingSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Parking
-        fields = ['parking_id', 'name', 'description', 'location']
-'''
+        fields = ['sector_id', 'name', 'description','boundary', 'centroid']
