3009637da78043bd7d292e397b005fc0bd478002
mackmmo <moorem2360@gmail.com>
2026-03-17 12:18:11 -0600

    working on pycache

--- guide/views.py
+++ guide/views.py
@@ -1,22 +1,9 @@
 # guide/views.py
 from rest_framework import generics
-from .models import Sector, Area, SubArea, Routes
-from .serializers import SectorSerializer, AreaSerializer, SubAreaSerializer, RoutesSerializer
+from .models import Sector
+from .serializers import SectorSerializer
 from django.http import JsonResponse
 
 class SectorListView(generics.ListAPIView):
     queryset = Sector.objects.all().order_by('sector_id')
     serializer_class = SectorSerializer
-'''
-class AreaListView(generics.ListAPIView):
-    queryset = Area.objects.all().order_by('area_id')
-    serializer_class = AreaSerializer
-
-class SubAreaListView(generics.ListAPIView):
-    queryset = SubArea.objects.all().order_by('subarea_id')
-    serializer_class = SubAreaSerializer
-
-class RoutesListView(generics.ListAPIView):
-    queryset = Routes.objects.all().order_by('route_id')
-    serializer_class = RoutesSerializer
-'''
