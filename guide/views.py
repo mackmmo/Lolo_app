b270f9419c0769e3b71b6a0bb97a4fd92b3657da
mackmmo <moorem2360@gmail.com>
2026-04-08 13:39:26 -0600

    added SubAreaSerializer and SubAreaListView

--- guide/views.py
+++ guide/views.py
@@ -1,7 +1,7 @@
 # guide/views.py
 from rest_framework import generics
-from .models import Sector, Area
-from .serializers import SectorSerializer, AreaSerializer
+from .models import Sector, Area, SubArea
+from .serializers import SectorSerializer, AreaSerializer, SubAreaSerializer
 from django.http import JsonResponse
 
 class SectorListView(generics.ListAPIView):
@@ -11,3 +11,8 @@ class SectorListView(generics.ListAPIView):
 class AreaListView(generics.ListAPIView):
     queryset = Area.objects.all().order_by('area_id')
     serializer_class = AreaSerializer
+
+class SubAreaListView(generics.ListAPIView):
+    queryset = SubArea.objects.all().order_by('subarea_id')
+    serializer_class = SubAreaSerializer
+    
