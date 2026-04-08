9b319a1e82d6db2fd6bc77a4c69b5a040ae879fd
mackmmo <moorem2360@gmail.com>
2026-04-08 14:17:42 -0600

    changes to serializers

--- guide/serializers.py
+++ guide/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Sector, Area
+from .models import Sector, Area, SubArea
 
 class SectorSerializer(serializers.ModelSerializer):
     class Meta:
@@ -9,4 +9,9 @@ class SectorSerializer(serializers.ModelSerializer):
 class AreaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Area
-        fields = ['area_id', 'sector', 'boundary', 'centroid', 'name', 'description']
+        fields = ['area_id', 'sector', 'boundary', 'centroid', 'name', 'description', 'directions', 'approach_time', 'drive_time', 'aspect']
+
+class SubAreaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SubArea
+        fields = ['subarea_id', 'area', 'centroid', 'name', 'description', 'aspect']
